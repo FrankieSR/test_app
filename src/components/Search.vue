c14093f3f31183d583ffe0b9800b087b6e8b7954
Artem Doroshko <artemdoroshko555@gmail.com>
2019-05-17 00:23:58 +0300

    add search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="search-block" ref="search" v-if="isAuthenticated">
-    <input type="text" v-model="search">
-    <ul>
-      <li v-for="(question, i) in todosByTitle" :key="i">{{ question }}</li>
+    <div class="input-group">
+      <input type="text" v-model.lazy="search">
+      <button @click="searchQuestion()">Search</button>
+    </div>
+    <ul v-if="inputTextLenght">
+      <li v-for="(question, i) in sortedList" :key="i">
+        <h3 @click="showSearchBlock">{{question.question}}</h3>
+        <ol v-show="show">
+          <li v-for="(option, i) in question.choise" :key="i*10">{{option.option}}</li>
+        </ol>
+      </li>
     </ul>
   </div>
 </template>
@@ -16,8 +24,10 @@ export default {
   props: {},
   data: function() {
     return {
-      search: " ",
-      questions: []
+      search: "",
+      questions: [],
+      show: false,
+      sortedList: []
     };
   },
   methods: {
@@ -28,15 +38,35 @@ export default {
           this.questions.push(element);
         }
       }
+    },
+
+    showSearchBlock() {
+      this.show = !this.show;
+    },
+    searchQuestion() {
+      this.sortedList = this.questions.filter(
+        item => item.question.indexOf(this.search) !== -1
+      );
     }
   },
+  filters: {},
   computed: {
     ...mapGetters(["getProfile", "isAuthenticated", "isProfileLoaded"]),
     ...mapState({
       authLoading: state => state.authStatus === "loading"
     }),
     todosByTitle() {
-      return this.questions.filter(item => item.question.indexOf(this.search) !== -1);
+
+    },
+    inputTextLenght() {
+      console.log(this.search.length);
+
+      if (this.search.length < 2) {
+        console.log(this.search);
+        return false;
+      }
+
+      return true;
     }
   },
   mounted() {
